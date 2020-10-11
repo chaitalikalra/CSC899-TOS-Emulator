95b34c0a40753b6ea56efabd2f1820682ba9668e
Chaitali Kalra <chaitalikalra@gmail.com>
2020-10-11 03:59:49 -0700

    Expose AssembledProgram from main.ts

--- x86/main/main.ts
+++ x86/main/main.ts
@@ -1,2 +1,5 @@
-export { Assembler as x86Assembler } from "../x86Assembler/src/assembler";
+export {
+    Assembler as x86Assembler,
+    AssembledProgram as x86AssembledProgram,
+} from "../x86Assembler/src/assembler";
 export { PC as x86PC } from "../x86PC/src/pc";
