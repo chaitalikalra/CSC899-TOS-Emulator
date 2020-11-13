8d730997c88dfa334cb8824fbd94d1295ab1f31d
Chaitali Kalra <chaitalikalra@gmail.com>
2020-11-13 10:12:33 -0800

    Add hoverable popover to show 16 bit and 8 bit registers

--- webapp/src/app/registers-view/registers-view.component.ts
+++ webapp/src/app/registers-view/registers-view.component.ts
@@ -12,11 +12,23 @@ export class RegistersViewComponent implements OnChanges {
     'ebx',
     'ecx',
     'edx',
+    'ebp',
     'esi',
     'edi',
     'esp',
     'eip',
   ];
+
+  static readonly RegisterPopOver = {
+    eax: ['ax', 'al', 'ah'],
+    ebx: ['bx', 'bl', 'bh'],
+    ecx: ['cx', 'cl', 'ch'],
+    edx: ['dx', 'dl', 'dh'],
+    ebp: ['bp'],
+    esi: ['si'],
+    edi: ['di'],
+    esp: ['sp'],
+  };
 
   static readonly DefaultValue = '';
 
@@ -28,12 +40,32 @@ export class RegistersViewComponent implements OnChanges {
     return RegistersViewComponent.Registers32Bits;
   }
 
+  getPopoverRegisters(reg: string): string[] {
+    if (RegistersViewComponent.RegisterPopOver[reg] !== undefined) {
+      return RegistersViewComponent.RegisterPopOver[reg];
+    } else {
+      return [];
+    }
+  }
+
+  getPopoverContent(reg: string): string {
+    const popoverRegs = this.getPopoverRegisters(reg);
+    const ret: string[] = [];
+    for (const r of popoverRegs) {
+      const value =
+        this.x86Service.executionContext === null
+          ? this.defaultValue
+          : this.x86Service.executionContext.registers[r];
+      ret.push(r + ': ' + value);
+    }
+    return ret.join('<br />');
+  }
+
   constructor(public x86Service: X86Service) {
     console.log('Register View Constructor');
   }
 
   ngOnChanges(s: SimpleChanges): void {
-
     console.log('onChange');
   }
 }
