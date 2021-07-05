5a509c9ee0ffb6830f2a55ef555349cbbd9517dc
Chaitali Kalra <chaitalikalra@gmail.com>
2021-07-05 03:04:18 -0700

    Add E2E tests for the webapp

--- webapp/e2e/src/app.e2e-spec.ts
+++ webapp/e2e/src/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import { browser, logging, } from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -8,9 +8,41 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('app title should be on top', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('webapp app is running!');
+    expect(page.getAppTitle()).toEqual('TOS x86 Emulator');
+  });
+
+  it('clear button should clear browser', () => {
+    page.navigateTo();
+    expect(page.getCodeInEditor()).toEqual('Type some code here');
+    page.clearCode().then(() => {
+      expect(page.getCodeInEditor()).toEqual('');
+    });
+  });
+
+  it('code from uploaded file should be added in code editor', () => {
+    page.navigateTo();
+    page.uploadInvalidCode().then(() => {
+      expect(page.getCodeInEditor()).toEqual('this is a test file');
+    })
+  });
+
+  it('invalid code will not assemble', () => {
+    page.navigateTo();
+    page.uploadInvalidCode().then(() => {
+      return page.assembleCode(); 
+    }).then(() => {
+      expect(page.isAssembleError()).toEqual(true);
+    });
+  });
+
+  it('check buttons on assemble page', () => {
+    page.navigateTo();
+    expect(page.isClearButtonDisplayed()).toEqual(true);
+    expect(page.isAssembleButtonDisplayed()).toEqual(true);
+    expect(page.isBrowseButtonDisplayed()).toEqual(true);
+    expect(page.isSlideExampleDropdownDisplayed()).toEqual(true);
   });
 
   afterEach(async () => {
--- webapp/e2e/src/app.po.ts
+++ webapp/e2e/src/app.po.ts
@@ -1,11 +1,59 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, WebElement } from 'protractor';
+
+var path = require('path');
 
 export class AppPage {
   navigateTo(): Promise<unknown> {
     return browser.get(browser.baseUrl) as Promise<unknown>;
   }
 
-  getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+  getAppTitle(): Promise<string> {
+    return element(by.id('app-title')).getText() as Promise<string>;
+  }
+
+  getCodeInEditor(): Promise<string> {
+    return element(by.css('.ace_content')).getText() as Promise<string>;
+  }
+
+  clearCode(): Promise<void> {
+    return element(by.css('.clear-button')).click() as Promise<void>;
+  }
+
+  getUploadElement(): WebElement {
+    let uploadInput = element(by.css("input[type=file]"));
+    browser.executeScript(
+      "arguments[0].style.visibility = 'visible'; arguments[0].style.height = '1px'; arguments[0].style.width = '1px'; arguments[0].style.opacity = 1",
+      uploadInput);
+    return uploadInput;
+  }
+
+  uploadInvalidCode(): Promise<void> {
+    let uploadInput = this.getUploadElement();
+    let absolutePath = path.resolve('e2e/resources/test_invalid_code.txt');
+    return uploadInput.sendKeys(absolutePath) as Promise<void>;
+  }
+
+  assembleCode(): Promise<void> {
+    return element(by.css('.assemble-button')).click() as Promise<void>;
+  }
+
+  isAssembleError(): Promise<boolean> {
+    return element(by.css('.alert-danger')).isDisplayed() as Promise<boolean>;
+  }
+
+  isClearButtonDisplayed(): Promise<boolean> {
+    return element(by.css('.clear-button')).isDisplayed() as Promise<boolean>;
+  }
+
+  isAssembleButtonDisplayed(): Promise<boolean> {
+    return element(by.css('.assemble-button')).isDisplayed() as Promise<boolean>;
+  }
+
+  isBrowseButtonDisplayed(): Promise<boolean> {
+    return element(by.css('.browse-button')).isDisplayed() as Promise<boolean>;
+  }
+
+  isSlideExampleDropdownDisplayed(): Promise<boolean> {
+    return element(by.css('.slide-examples-dropdown')).isDisplayed() as Promise<boolean>;
   }
 }
