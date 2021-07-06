3c3a889fafd4bdb0a8c1abca46ea7e30c022b1e8
Abhinav Arora <abhinavarora@fb.com>
2021-07-05 20:36:57 -0700

    Add unit tests to webapp

--- webapp/src/app/assemble/assemble.component.spec.ts
+++ webapp/src/app/assemble/assemble.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { AssembleComponent } from './assemble.component';
 
@@ -8,7 +9,8 @@ describe('AssembleComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AssembleComponent ]
+      declarations: [ AssembleComponent ],
+      imports: [ RouterTestingModule ],
     })
     .compileComponents();
   });
